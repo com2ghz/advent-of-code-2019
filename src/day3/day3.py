1e37d4184676da8b402a74595378bac2e4598acf
com2ghz <gokhan@orhun.nl>
2019-12-04 21:59:23 +0100

    Day 3 part 2done

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -11,7 +11,27 @@ def main():
 
         manhattan_map = calculate_manhattan(intersecting)
         shortest_distance = min(manhattan_map.values())
-        print(shortest_distance)
+        print("Part 1: Shortest distance: ", shortest_distance)
+
+        wire1_steps = calculate_step_count(intersecting, wire_walk1)
+        wire2_steps = calculate_step_count(intersecting, wire_walk2)
+        sums = sum_wire_steps(wire1_steps, wire2_steps)
+        print("Part 2: Least steps for the first intersection:", min(sums))
+
+
+def sum_wire_steps(wire1_steps, wire2_steps):
+    sums = []
+    for i in range(len(wire1_steps)):
+        sums.append(wire1_steps[i] + wire2_steps[i])
+    return sums
+
+
+def calculate_step_count(intersecting, wire_walk):
+    steps = []
+    for point in intersecting:
+        count = wire_walk.index(point) + 1
+        steps.append(count)
+    return steps
 
 
 def calculate_manhattan(intersecting):
@@ -48,17 +68,24 @@ def walkwire(actions):
                 y_pos = y_pos + 1
             elif direction == "D":
                 y_pos = y_pos - 1
-            wire_walk.append(point(x_pos, y_pos))
+            wire_walk.append(Point(x_pos, y_pos))
     return wire_walk
 
 
-class point:
+class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
+
+    def __eq__(self, other):
+        if not isinstance(other, Point):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.x == other.x and self.y == other.y
 
 
 if __name__ == '__main__':
